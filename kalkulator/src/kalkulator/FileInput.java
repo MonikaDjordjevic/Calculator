4c8310ee0687bd682243f23ea7947ee74a0d302e
MonikaDjordjevic <monikadjordjevic1993@gmail.com>
2017-03-19 15:07:57 +0100

    Possibility to pass file name to function

--- kalkulator/src/kalkulator/FileInput.java
+++ kalkulator/src/kalkulator/FileInput.java
@@ -1,25 +1,25 @@
 package kalkulator;
 import java.util.Scanner;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
-
 public class FileInput implements Input {
 
-	public static final String FILENAME = "C:\\Users\\Tomasz\\workspace\\kalkulator\\inputs\\test.txt.txt";
+	public static final String PATH = "inputs\\";
 
 	
 	public void promptInstructions(){
 		System.out.println("============================");
-        System.out.println("|  Write your equotations  |");
+        System.out.println("|       Add your file      |");
         System.out.println("============================");
         System.out.println("|                          |");
-        System.out.println("|  Use only numbers and    |");
-        System.out.println("|        symbols + - * /   |");
+        System.out.println("|  Place file in inputs    |");
+        System.out.println("|        directory         |");
         System.out.println("|                          |");
-        System.out.println("|  When you want to finish |");
-        System.out.println("|     write  koniec        |");
+        System.out.println("|  And write down its name |");
+        System.out.println("|     with extension       |");
         System.out.println("|                          |");
         System.out.println("|                          |");
         System.out.println("============================");	
@@ -29,28 +29,31 @@ public class FileInput implements Input {
 	
 	public String[] getInput(){
 		
+		Scanner scanner = new Scanner(System.in);
+		String file = scanner.nextLine();
+		
+		String filename = PATH + file;
+		
 		
 		BufferedReader br = null;
 		FileReader fr = null;
 		DynamicStringArray result = new DynamicStringArray();
 		
-		try {
-
-			fr = new FileReader(FILENAME);
-			br = new BufferedReader(fr);
-
+		try{
 			String sCurrentLine;
 
-			br = new BufferedReader(new FileReader(FILENAME));
-
+			br = new BufferedReader(new FileReader(new File(filename)));
+			
 			while ((sCurrentLine = br.readLine()) != null) {
 				result.add(sCurrentLine);
 			}
-
+			
+			fr = new FileReader(filename);
+			br = new BufferedReader(fr);
 		} catch (IOException e) {
 
 			e.printStackTrace();
-
+			
 		} finally {
 
 			try {
@@ -69,8 +72,6 @@ public class FileInput implements Input {
 
 		}
 		
-		
-		return result.toArray();
-
+		return result.toArray();	
 	}
 }
