fadf57227108d01cd7c715926548a566ddcfe922
MonikaDjordjevic <monikadjordjevic1993@gmail.com>
2017-03-19 17:05:06 +0100

    Removing tests from Main

--- kalkulator/src/kalkulator/Main.java
+++ kalkulator/src/kalkulator/Main.java
@@ -2,22 +2,7 @@ package kalkulator;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		Evaluable z = new Singleton(5);
-		Evaluable x = new Singleton(3);
-		Evaluable c = new Multiply(z, x);
-		Evaluable v = new Add(z, c);
-		
-		System.out.println(v.evaluate());
-		
-		String test = "2+ 3 *2 -5 +10*2";
-		//String test = "2+3*2-5";
-		
-		Evaluable q = Parser.parse(test);
-		System.out.println(q.evaluate());
-		
+	public static void main(String[] args) {		
 		Menu menu = new Menu();
 		
 		while(true){
